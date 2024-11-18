a96d39a4c5c67c8e6193207e71b1e02c3b2d4dc7
rohit prasain <workstuffrohit@gmail.com>
2024-11-18 01:25:58 +0000

    chore: replaced error handling logic in eventController

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,48 +1,58 @@
 async function getEvents(req, res, next) {
   try {
     const data = [{ id: 1, name: "first event" }];
-    // throw new Error("failed");
     await res.status(200).json(data, "Event fetched successfully");
   } catch (error) {
-    console.error("Error while fetching events", error.message);
+    console.error("Err", error.message);
     res.status(500).json({ devMessage: error.message }, "Could not get events");
+  } finally {
     next();
   }
 }
 
 async function postEvent(req, res, next) {
   try {
-    await res.json([
-      {
-        id: 2,
-        name: "second event posted",
-      },
-    ]);
+    const data = req.body;
+    //post event
+    await res.status(200).json(data, "Event posted successfully");
   } catch (error) {
-    console.error("Errror while fetching events", error.message);
+    console.error("Err", error.message);
+    res
+      .status(500)
+      .json({ devMessage: error.message }, "Could not post events");
+  } finally {
     next();
   }
 }
 
 async function putEvent(req, res, next) {
   try {
-    await res.json({
-      status: 200,
-      data: "list of events",
-    });
+    const eventId = req.params.id;
+    const data = req.body;
+    //update event
+    await res.status(200).json(data, "Event updated successfully");
   } catch (error) {
-    console.error("Errror while fetching events", error.message);
+    console.error("Err", error.message);
+    res
+      .status(500)
+      .json({ devMessage: error.message }, "Could not update event");
+  } finally {
     next();
   }
 }
 
 async function deleteEvent(req, res, next) {
-  const eventId = req.params.id;
-  console.log("here", req.params);
   try {
-    res.json("event deleted successfully" + eventId);
+    const eventId = req.params.id;
+    //delete event
+    // throw new Error("failed");
+    await res.status(200).json(data, "Event deleted successfully");
   } catch (error) {
-    console.error("Errror while fetching events", error.message);
+    console.error("Err", error.message);
+    res
+      .status(500)
+      .json({ devMessage: error.message }, "Could not delete event");
+  } finally {
     next();
   }
 }
